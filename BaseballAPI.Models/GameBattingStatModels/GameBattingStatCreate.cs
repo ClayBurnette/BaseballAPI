5d7fd15a3e3c6fedf65e1f9abede69c2bf220cb2
William Lego <william.a.lego11@gmail.com>
2022-04-10 15:48:52 -0400

    GBS Service and Controller Functions

---
+++ BaseballAPI.Models/GameBattingStatModels/GameBattingStatCreate.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseballAPI.Models.GameBattingStatsModels
+{
+    public class GameBattingStatCreate
+    {
+        [Required]
+        public int PlayerId { get; set; }
+        [Required]
+        public int GameId { get; set; }
+        [Required]
+        public int AB { get; set; }
+        [Required]
+        public int R { get; set; }
+        [Required]
+        public int H { get; set; }
+        [Required]
+        public int RBI { get; set; }
+        [Required]
+        public int BB { get; set; }
+        [Required]
+        public int SO { get; set; }
+        [Required]
+        public int HR { get; set; }
+        [Required]
+        [Display(Name = "2B")]
+        public int Double { get; set; }
+        [Required]
+        [Display(Name = "3B")]
+        public int Triple { get; set; }
+        [Required]
+        public int SAC { get; set; }
+        [Required]
+        public int HBP { get; set; }
+        [Required]
+        public int SB { get; set; }
+        [Required]
+        public int CS { get; set; }
+        public GameBattingStatCreate(int playerId, int gameId, int ab, int r, int h, int rbi, int bb, int so, int hr, int doub, int trip, int sac, int hbp, int sb, int cs)
+        {
+            PlayerId = playerId;
+            GameId = gameId;
+            AB = ab;
+            R = r;
+            H = h;
+            RBI = rbi;
+            BB = bb;
+            SO = so;
+            HR = hr;
+            Double = doub;
+            Triple = trip;
+            SAC = sac;
+            HBP = hbp;
+            SB = sb;
+            CS = cs;
+        }
+        public GameBattingStatCreate(string allStats)
+        {
+            string[] statArray = allStats.Split('/');
+            if (statArray.Length == 15)
+            {
+                int[] intConvert = Array.ConvertAll(statArray, s => int.Parse(s));
+                PlayerId = intConvert[0];
+                GameId = intConvert[1];
+                AB = intConvert[2];
+                R = intConvert[3];
+                H = intConvert[4];
+                RBI = intConvert[5];
+                BB = intConvert[6];
+                SO = intConvert[7];
+                HR = intConvert[8];
+                Double = intConvert[9];
+                Triple = intConvert[10];
+                SAC = intConvert[11];
+                HBP = intConvert[12];
+                SB = intConvert[13];
+                CS = intConvert[14];
+            }
+        }
+    }
+}
---
+++ BaseballAPI.WebAPI/Controllers/GameBattingStatController.cs
@@ -0,0 +1,66 @@
+﻿using BaseballAPI.Models.GameBattingStatsModels;
+using BaseballAPI.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BaseballAPI.WebAPI.Controllers
+{
+    public class GameBattingStatController : ApiController
+    {
+        public IHttpActionResult Post(GameBattingStatCreate model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var service = new GameBattingStatService();
+            if (!service.CreateGameBattingStat(model))
+                return InternalServerError();
+            return Ok();
+        }
+        public IHttpActionResult Get()
+        {
+            var service = new GameBattingStatService();
+            var stats = service.GetAllGameBattingStats();
+            return Ok(stats);
+        }
+        public IHttpActionResult Get(string queryType, int id)
+        {
+            var service = new GameBattingStatService();
+            if (queryType.ToLower() == "stat")
+            {
+                var stats = service.GetGameBattingStat(id);
+                return Ok(stats);
+            }
+            if (queryType.ToLower() == "game")
+            {
+                var stats = service.GetGBSListByGameId(id);
+                return Ok(stats);
+            }
+            if (queryType.ToLower() == "player")
+            {
+                var stats = service.GetGBSListByPlayerId(id);
+                return Ok(stats);
+            }
+            return BadRequest("Invalid query type (stat, game, player)");
+        }
+        public IHttpActionResult Put(GameBattingStatEdit stat)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var service = new GameBattingStatService();
+            if (!service.UpdateGameBattingStat(stat))
+                return InternalServerError();
+            return Ok();
+        }
+        public IHttpActionResult Delete(int id)
+        {
+            var service = new GameBattingStatService();
+            if (!service.DeleteGameBattingStat(id))
+                return InternalServerError();
+            return Ok();
+        }
+    }
+}
