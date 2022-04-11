37b37e05454aa99e0bf50ccd7b629a4d6137c791
chris pettigrew <cdpettigrew26@comcast.net>
2022-04-11 11:32:28 -0400

    Tweaks to player

--- BaseballAPI.Data/Player.cs
+++ BaseballAPI.Data/Player.cs
@@ -13,7 +13,7 @@ namespace BaseballAPI.Data
     {
         [Key]
         public int PlayerId { get; set; }
-        [Required]
+        
         public Guid OwnerId { get; set; }
         [Required]
         public string Name { get; set; }
