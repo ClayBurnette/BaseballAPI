0d78d76ab745bdb418c8e6f3eadcb62ecf44658a
chris pettigrew <cdpettigrew26@comcast.net>
2022-04-13 10:15:35 -0400

    AddedSeedContent

--- BaseballAPI.Data/Migrations/Configuration.cs
+++ BaseballAPI.Data/Migrations/Configuration.cs
@@ -17,8 +17,56 @@
         {
             //  This method will be called after migrating to the latest version.
             context.Teams.AddOrUpdate(x => x.TeamId,
-               new Team() { TeamId = 1 , TeamName = "Arizona Diamondbacks", TeamLocation = "Phoenix, Arizona", TeamMascot = "Timmy", TeamStadium = "Phoenix Stadium" }
+               new Team() { TeamId = 1 ,TeamName = "Arizona Diamondbacks", TeamLocation = "Phoenix, Arizona", TeamMascot = "Timmy", TeamStadium = "Phoenix Stadium" }
                 );
+            context.Players.AddOrUpdate(p => p.PlayerId, new Player()
+            {
+                PlayerId = 1,
+                Name = "Chris",
+                DateOfBirth = "06/28/1999",
+                School = "IvyTech",
+                PlayerPosition = Position.Catcher,
+                JerseyNumber = 26,
+                HomeTown = "Speedway",
+                TeamId = 1
+            }
+            );
+            context.Players.AddOrUpdate(p => p.PlayerId, new Player()
+            {
+                PlayerId = 2,
+                Name = "Clay",
+                DateOfBirth = "07/04/1776",
+                School = "NotSure",
+                PlayerPosition = Position.FirstBase,
+                JerseyNumber = 20,
+                HomeTown = "Bloomington",
+                TeamId = 2
+            }
+            );
+            context.Players.AddOrUpdate(p => p.PlayerId, new Player()
+            {
+                PlayerId = 3,
+                Name = "William",
+                DateOfBirth = "11/20/1526",
+                School = "NotSureAgain",
+                PlayerPosition = Position.Pitcher,
+                JerseyNumber = 15,
+                HomeTown = "SomeWhere",
+                TeamId = 3
+            }
+            );
+            context.Players.AddOrUpdate(p => p.PlayerId, new Player()
+            {
+                PlayerId = 4,
+                Name = "Stephen",
+                DateOfBirth = "08/05/1125",
+                School = "NotSureOnceAgain",
+                PlayerPosition = Position.ShortStop,
+                JerseyNumber = 10,
+                HomeTown = "SomeWhereElse",
+                TeamId = 4
+            }
+            );
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method
             //  to avoid creating duplicate seed data.
         }
--- BaseballAPI.Data/Player.cs
+++ BaseballAPI.Data/Player.cs
@@ -13,8 +13,7 @@ namespace BaseballAPI.Data
     {
         [Key]
         public int PlayerId { get; set; }
-        
-        public Guid OwnerId { get; set; }
+        //public Guid OwnerId { get; set; }
         [Required]
         public string Name { get; set; }
         [Required]
