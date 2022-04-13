a48ec55d753f0d82dfb93c917b4713c5ac989223
Stephen Reilly <smreilly22@gmail.com>
2022-04-13 10:14:40 -0400

    Added Seed Data

--- BaseballAPI.Data/Migrations/Configuration.cs
+++ BaseballAPI.Data/Migrations/Configuration.cs
@@ -17,8 +17,12 @@
         {
             //  This method will be called after migrating to the latest version.
             context.Teams.AddOrUpdate(x => x.TeamId,
-               new Team() { TeamId = 1 , TeamName = "Arizona Diamondbacks", TeamLocation = "Phoenix, Arizona", TeamMascot = "Timmy", TeamStadium = "Phoenix Stadium" }
-                );
+               new Team() { TeamId = 1, TeamName = "Arizona Diamondbacks", TeamLocation = "Phoenix, Arizona", TeamMascot = "Timmy", TeamStadium = "Phoenix Stadium" },
+               new Team() { TeamId = 2, TeamName = "Atlanta Braves", TeamLocation = "Atlanta, GA", TeamMascot = "Homer", TeamStadium = "Sun Trust Stadium" }
+                ) ;
+
+            context.Games.AddOrUpdate(x => x.GameId,
+                new GameInfo() { GameId = 1, TeamId1 = 1, TeamId2 = 2, HomeScore = 6, AwayScore = 2, Innings = 9, SeasonYear = 2022 });
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method
             //  to avoid creating duplicate seed data.
         }
