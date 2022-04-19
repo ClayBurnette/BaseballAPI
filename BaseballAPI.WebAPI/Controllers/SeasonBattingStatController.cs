96a03192cb0f4df64be7e1ab1393d668bdec0ab3
William Lego <william.a.lego11@gmail.com>
2022-04-19 13:04:29 -0400

    Stats Documentation

--- BaseballAPI.WebAPI/Controllers/SeasonBattingStatController.cs
+++ BaseballAPI.WebAPI/Controllers/SeasonBattingStatController.cs
@@ -10,6 +10,14 @@ namespace BaseballAPI.WebAPI.Controllers
 {
     public class SeasonBattingStatController : ApiController
     {
+        //Get API Documentation
+        /// <summary>
+        /// This will return the season stats for an player using the player's ID.<br/>
+        /// To get the careet stats for a player, use '0' for the year.
+        /// </summary>
+        /// <param name="player"></param>
+        /// <param name="year"></param>
+        /// <returns></returns>
         public IHttpActionResult Get(int player, int year)
         {
             var service = new GameBattingStatService();
